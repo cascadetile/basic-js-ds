792ab1dfc7790590042be78d2087f8c89d556af7
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-10-18 00:17:56 +0600

    feat: add some methods of a binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,9 +8,12 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.head = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
   add(/* data */) {
