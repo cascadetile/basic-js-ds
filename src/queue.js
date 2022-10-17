a4fe14ad3f9728a8254eb47f1beee65e6f0491a1
cascadetile <112803211+cascadetile@users.noreply.github.com>
2022-10-17 23:39:52 +0600

    feat: add queue solution

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor() {
+    this.queue = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.queue;
+  }
+
+  enqueue(value) {
+    if (!this.queue) {
+      this.queue = new ListNode(value);
+    } else {
+      let current = this.queue;
+      while (current) {
+        if (current.next) {
+          current = current.next;
+        } else {
+          current.next = new ListNode(value);
+          break;
+        }
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.queue) return;
+    let value = this.queue.value;
+    this.queue = this.queue.next;
+    return value;
   }
 }
 
